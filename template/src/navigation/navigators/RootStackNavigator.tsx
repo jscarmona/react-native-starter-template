3341351fa861dabeb37bacb469f2ee26863eb87a
Javier Carmona <javierscarmona@gmail.com>
2023-03-02 11:08:02 -0500

    feat: move not found screen into own component

--- template/src/navigation/navigators/RootStackNavigator.tsx
+++ template/src/navigation/navigators/RootStackNavigator.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
 import { RootStackParamList } from '../types/RootStackParamList';
 import { NavigationRoute } from '../constants/navigation';
+import { NotFoundScreen } from '../screens/NotFoundScreen';
 import { RootBottomTabNavigator } from './RootBottomTabNavigator';
-
-function NotFoundScreen() {
-  return <Text>Not Found</Text>;
-}
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
---
+++ template/src/navigation/screens/NotFoundScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Screen } from '../components/Screen';
+
+export function NotFoundScreen() {
+  return (
+    <Screen>
+      <Text>Not Found</Text>
+    </Screen>
+  );
+}
