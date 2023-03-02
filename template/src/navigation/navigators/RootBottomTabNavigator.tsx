4088cfa7471cce0fe8545f9a495ed20e34c1deff
Javier Carmona <javierscarmona@gmail.com>
2023-03-02 11:03:15 -0500

    fix(navigation): show tab bar label

--- template/src/navigation/navigators/RootBottomTabNavigator.tsx
+++ template/src/navigation/navigators/RootBottomTabNavigator.tsx
@@ -7,7 +7,7 @@ const BottomTab = createBottomTabNavigator<RootBottomTabParamList>();
 
 export function RootBottomTabNavigator() {
   return (
-    <BottomTab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <BottomTab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
       <BottomTab.Screen name="Home" component={HomeScreen} />
     </BottomTab.Navigator>
   );
